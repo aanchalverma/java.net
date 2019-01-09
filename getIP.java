0d867c83108a2e1b0303a875d402b8396544c8c3
aanchalverma <34161377+aanchalverma@users.noreply.github.com>
2019-01-09 12:26:18 +0530

    Update getIP.java

--- getIP.java
+++ getIP.java
@@ -1,15 +1,26 @@
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.net.Socket;
 
 public class GetIP
 {
 	public static void main(String args[])
     {
     	InetAddress ipadd = null;
+      	InetAddress add = null;
       	try
         {
         	//add = InetAddress.getByName("https://chilliteen.wordpress.com/2018/04/14/rapism");
           	ipadd = InetAddress.getLocalHost();
+          
+          
+          // web socket
+          Socket sock = new Socket("https://www.compilejava.net/",80);
+          add = sock.getInetAddress();
+          System.out.println("Connected to " + add);
+         sock.close();
+
+          
 
           	
         }
@@ -17,8 +28,16 @@ public class GetIP
         {
          System.exit(2);
       	}
+      	catch (java.io.IOException e)
+        {
+         System.out.println("Can't connect to " + args[0]);
+         System.out.println(e);
+        }
+
+      
+      
 		System.out.println(ipadd.getHostName() + "=" + ipadd.getHostAddress());
-      System.exit(0);
+      	System.exit(0);
 
     
     }
